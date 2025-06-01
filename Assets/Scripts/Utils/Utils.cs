6e917bbb346e58b8beb0030ff52856053c539c1c
superjoung <46063485+superjoung@users.noreply.github.com>
2025-06-02 02:58:02 +0900

    FEAT : 완

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -64,7 +64,6 @@ namespace Util
                                                                    // 모바일 or 에디터 마우스 터치좌표 처리
         public static void TouchSetUp(ref TouchState touchState, ref Vector2 touchPosition)
         {
-#if UNITY_EDITOR
             if (Input.GetMouseButtonDown(0))
             {
                 if (EventSystem.current.IsPointerOverGameObject() == false) { touchState = TouchState.Began; }
@@ -73,19 +72,6 @@ namespace Util
             else if (Input.GetMouseButtonUp(0)) { if (EventSystem.current.IsPointerOverGameObject() == false) { touchState = TouchState.Ended; } }
             else touchState = TouchState.None;
             touchPosition = Input.mousePosition;
-#else
-                if (Input.touchCount > 0)
-                {
-
-                    Touch touch = Input.GetTouch(0);
-                    if (EventSystem.current.IsPointerOverGameObject(touch.fingerId) == true) return;
-                    if (touch.phase == TouchPhase.Began) touchState = ETouchState.Began;
-                    else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) touchState = ETouchState.Moved;
-                    else if (touch.phase == TouchPhase.Ended) if (touchState != ETouchState.None) touchState = ETouchState.Ended;
-                    touchPosition = touch.position;
-                }
-                else touchState = ETouchState.None;
-#endif
         }
     }
 }
