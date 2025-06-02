78d58ee70136dcee5af3770904f0d5eed941da39
superjoung <46063485+superjoung@users.noreply.github.com>
2025-06-03 02:10:27 +0900

    HOTFIX : 증거 수집시 상단에 증거 이미지 추가

--- Assets/Scripts/Player/PlayerEvidenceMove.cs
+++ Assets/Scripts/Player/PlayerEvidenceMove.cs
@@ -16,11 +16,17 @@ public class PlayerEvidenceMove : MonoBehaviour
     private List<GameObject> _getEvidences = new List<GameObject>();
     private Animator _animator;
     private bool _canFind = false;
+    private PlayerMainScreenUI _mainScreen;
 
     private void Awake()
     {
         _agent = transform.GetComponent<NavMeshAgent>();
         _animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        _mainScreen = GameObject.Find("PlayerMainScreenUI").GetComponent<PlayerMainScreenUI>();
     }
 
     private void Update()
@@ -48,6 +54,7 @@ public class PlayerEvidenceMove : MonoBehaviour
                 Destroy(data);
                 GameManager.Instance.EvidenceInventory.Add(data.EvidenceID);
                 _getEvidences[0].tag = "TempEvidence";
+                _mainScreen.InputEvidenceData(data.EvidenceID);
                 _getEvidences.RemoveAt(0);
             }
             else if(_canFind)
